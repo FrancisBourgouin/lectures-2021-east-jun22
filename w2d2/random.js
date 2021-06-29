11003936a1e689bc387a7e3475ed6f0afe183cd3
Francis Bourgouin <netbeuix@gmail.com>
2021-06-29 13:32:16 -0400

    add w2d2 lecture

---
+++ w2d2/random.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+  console.log('sup')
+})
+
+const array = [1, 2, 3]
+
+array.forEach(() => {
+  console.log('sup')
+})
+
+for (const element of array) {
+  console.log('sup')
+}
